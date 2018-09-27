e5f565f454fe220403c35add265e179837500e0f
Joker20000 <38452925+Joker20000@users.noreply.github.com>
2018-09-27 22:14:59 +0800

    20180927

---
+++ src/pages/balance/share/share.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="share">
+    <div class="share_img">
+      <img src="../../../../static/images/share_img.png">
+    </div>
+    <div class="share_popup" v-show="bg_show">
+      <div class="share_bg" v-on:click="bg_show = false"></div>
+      <img src="../../../../static/images/share_click.png">
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'share',
+    data () {
+      return {
+        bg_show: false
+      }
+    },
+    mounted () {
+      if(this.getStorage('share')){
+        this.bg_show = true;
+        this.setStorage('share','');
+      }
+    }
+  }
+</script>
+<style lang="less" scoped>
+  @import "share.less";
+</style>
